168babc06fa5c1de895e6ef484c118f629f1b861
sleepingsloth0105 <6zzzzzz610@gmail.com>
2023-07-22 04:10:29 +0900

    긴급오류수정

--- test.py
+++ test.py
@@ -1,33 +1,11 @@
-from ClassMaplestorysixV2 import *
+a = {}
+a[1] = [11,22,33,44]
+a[2] = [55,66,77,88]
+k = a.items()
+print(a.items())
+for i in k:
+    if [11,22,33,44] in i:
+        print('in')
 
-six = CharacterV2(500,95)
-
-six.skill_addname('오리진')
-six.skill_adddamagepercent(0.0744)
-six.skill_adddamagerise('오리진', 820800 , 27360)
-
-six.mastery_addname('폭시')
-six.mastery_adddamagepercent( 0.2067 )
-six.mastery_adddamagerise('폭시' , 365 , 7)
-
-print(six.skillinfo.damage_rise10['오리진'])
-cnt = 1
-new = 1
-for i in range(29):
-    new *= six.skillinfo.damage_rise['오리진'][i]
-    cnt += 1
-    if cnt == 10:
-        print(new)
-        cnt = 0
-        new = 1
-    
-print(six.masteryinfo.damage_rise10['폭시'])
-cnt = 1
-new = 1
-for i in range(29):
-    new *= six.masteryinfo.damage_rise['폭시'][i]
-    cnt += 1
-    if cnt == 9 or cnt == 19 or cnt == 29 or cnt == 30:
-        print(new)
-        new = 1
-    
+findkey = [k for k,v in a.items() if v == [11,22,33,44,55]]
+print(findkey)
